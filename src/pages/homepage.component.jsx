7a92118fad3e8ee3fd5912da6168ec94c4702c3f
Abdul-Hammid Olagunju <akaniolagunju@yahoo.com>
2021-03-22 01:51:32 +0000

    broken homepage to smaller componets

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,8 @@
+import "./homepage.styles.scss";
+import Directory from "../components/directory/director.components";
+
+export const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
